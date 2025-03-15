48a6ebd0d7115c9a82a8238a3ce652ad806494f6
mortezai <morteza2006g@gmail.com>
2025-03-15 23:09:28 +0330

    hw09

---
+++ S09/hw/S09hw/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace S09hw;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s1 = new Student(
+            name: "morteza",
+            stdid: 10,
+            natid: 10,
+            credits: 17,
+            active: true
+        );
+        Console.WriteLine($"{s1.ToString()}");
+
+        string str = "rahim,11,11,21,false";
+        Student s2 = Student.Parse(str);
+        Console.WriteLine($"{s2.ToString()}");
+        File.WriteAllLines("students.csv", new string[]{s1.ToString(), s2.ToString()});
+
+        Console.WriteLine("------------------");
+        Student.students_list();
+
+        Console.WriteLine("------------------");
+        Student[] std_list = null;
+        Student.students_list(ref std_list);
+        Console.WriteLine($"{std_list[0].ToString()}");
+
+        Console.WriteLine("------------------");
+        Console.WriteLine($"{s2.Equals(s1)}");
+        Console.WriteLine($"{s2.Equals(std_list[0])}");
+    }
+}
---
+++ S09/hw/S09hw/Student.cs
@@ -0,0 +1,64 @@
+public class Student
+{
+    private string name;
+    private int stdid, natid, credits;
+    private bool active;
+
+    public Student(string name, int stdid, int natid, int credits, bool active)
+    {
+        this.name = name;
+        this.stdid = stdid;
+        this.natid = natid;
+        this.credits = credits;
+        this.active = active;
+    }
+
+    public override string ToString()
+    {
+        return $"{name}, {stdid}, {natid}, {credits}, {active}";
+    }
+
+    public static Student Parse(string str)
+    {
+        string[] tokens = str.Split(',');
+        return new Student(tokens[0], int.Parse(tokens[1]), int.Parse(tokens[2]), int.Parse(tokens[3]), bool.Parse(tokens[4]));
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Student)
+        {
+            Student other = obj as Student;
+            return 
+                name.Equals(other.name) &&
+                stdid.Equals(other.stdid) &&
+                natid.Equals(other.natid) &&
+                credits.Equals(other.credits) &&
+                active.Equals(other.active);
+        } else {
+            return false;
+        }
+    }
+
+    static public void students_list()
+    {
+        string[] lines = File.ReadAllLines("students.csv");
+        foreach(string line in lines)
+        {
+            string[] tokens = line.Split(',');
+            System.Console.WriteLine($"{tokens[0]},{tokens[1]},{tokens[2]},{tokens[3]},{tokens[4]}");
+        }
+    }
+
+    static public void students_list(ref Student[] std_list)
+    {
+        string[] lines = File.ReadAllLines("students.csv");
+        int i = 0, length = lines.Length;
+        std_list = new Student[length];
+        foreach(string line in lines)
+        {
+            string[] tokens = line.Split(',');
+            std_list[i] = new Student(tokens[0], int.Parse(tokens[1]), int.Parse(tokens[2]), int.Parse(tokens[3]), bool.Parse(tokens[4]));
+        }
+    }
+}
